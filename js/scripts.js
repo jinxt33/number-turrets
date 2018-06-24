52d175cd956c900a562afeae9cee081b74fcf7c6
Zoe Zhou <zoezo85@gmail.com>
2018-06-24 16:37:25 -0700

    it will separate business logic from ui logic, placing the loop calcs into a var function, then calling back to it within the submit listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
+//BIZ LOGIC
+var numArray = [];
+var arraySuper = [];
+var superLogic = function(inputNum) {
+  for (index = 1; index <= inputNum; index += 1) {
+    // index.includes('8');
+    if ( index % 8 === 0 ) {
+      numArray.push(index);
+      arraySuper.push("solucky!");
+      result = "solucky!";
+    } else if ( index % 4 === 0 ) {
+      numArray.push(index);
+      arraySuper.push("unlucky");
+      result = "unlucky";
+    } else if ( index % 3 === 0 ) {
+      numArray.push(index);
+      arraySuper.push("crazyyy");
+      result = "crazyyy";
+    } else {
+      numArray.push(index);
+      arraySuper.push(index);
+      result = index;
+    }
+    $("ul#results").append("<li>" + result + "</li>");
+  }
+}
+//UI LOGIC
 $(document).ready(function() {
-
-  var numArray = [];
-  var arraySuper = [];
-
   $("form#number").submit(function(event) {
     event.preventDefault();
-
-    var inputNum = $('input#number').val();
-    var index;
-
-    console.log(inputNum);
-    console.log(typeof inputNum);
-
-    for (index = 1; index <= parseInt(inputNum); index += 1) {
-
-      if ( index % 8 === 0 ) {
-        numArray.push(index);
-        arraySuper.push("solucky!");
-        result = "solucky!";
-      } else if ( index % 4 === 0 ) {
-        numArray.push(index);
-        arraySuper.push("unlucky");
-        result = "unlucky";
-      } else {
-        numArray.push(index);
-        arraySuper.push(index);
-        result = index;
-      }
-
-      $("ul#results").append("<li>" + result + "</li>");
-    }
-
-    console.log(numArray);
-    console.log(arraySuper);
-
+    var inputNum = parseInt($('input#number').val());
+    //CALLBACK
+    superLogic(inputNum);
+    //console.log(numArray);
+    //console.log(arraySuper);
   });
 });

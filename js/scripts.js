4ba85f1d3ea3c9666397d25aa84d6907a0943c99
Zoe Zhou <zoezo85@gmail.com>
2018-06-24 15:04:18 -0700

    it will force the user to enter a number via html input type, and it will create an array starting from 1 and split it into a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+  var numArray = [];
+  var arraySuper;
+
   $("form#number").submit(function(event) {
     event.preventDefault();
 
@@ -7,5 +10,12 @@ $(document).ready(function() {
 
     console.log(inputNum);
     console.log(typeof inputNum);
+
+    for (index = 1; index <= inputNum; index += 1) {
+      numArray.push(index);
+      $("ul#results").append("<li>" + index + "</li>");
+    }
+
+
   });
 });
